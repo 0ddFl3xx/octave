9faad511ac7b94029ef6446b7b5eacc3f291175d
0ddFl3xx <smithking072@gmail.com>
2025-02-08 10:01:08 +0200

    refactor: replace Plus icon with custom SVG

--- components/MobileNav.tsx
+++ components/MobileNav.tsx
@@ -1,4 +1,4 @@
-import { LibraryBig, Plus, House, Search } from "lucide-react";
+import { LibraryBig, House, Search } from "lucide-react";
 
 const MobileNav = () => {
   return (
@@ -18,7 +18,13 @@ const MobileNav = () => {
             Your library
           </div>
           <div className="flex flex-col items-center text-xs text-zinc-400">
-            <Plus className="size-6" />
+            <svg
+              xmlns="http://www.w3.org/2000/svg"
+              className="size-6 text-zinc-400"
+              fill="#868D98"
+              viewBox="0 0 256 256">
+              <path d="M224,128a8,8,0,0,1-8,8H136v80a8,8,0,0,1-16,0V136H40a8,8,0,0,1,0-16h80V40a8,8,0,0,1,16,0v80h80A8,8,0,0,1,224,128Z"></path>
+            </svg>
             Create
           </div>
         </div>
