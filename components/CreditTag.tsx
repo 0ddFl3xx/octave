f715b841bcdcb0912153249567ed737cd23d87cf
0ddFl3xx <smithking072@gmail.com>
2025-02-09 15:29:58 +0200

    claude suggested layout fix?

--- components/CreditTag.tsx
+++ components/CreditTag.tsx
@@ -2,7 +2,7 @@ import Link from "next/link";
 
 function CreditTag() {
   return (
-    <aside className="fixed top-4 end-4 z-10 flex items-center justify-center gap-4 rounded-lg bg-white/30 backdrop-filter backdrop-blur-lg px-5 py-3 text-white hover:scale-105 hover:">
+    <aside className="fixed top-4 end-4 z-10 flex items-center justify-center gap-4 rounded-lg bg-white/30 backdrop-filter backdrop-blur-lg px-5 py-3 text-white hover:scale-105">
       <Link
         href="https://github.com/0ddFl3xx/"
         target="_blank"
--- components/Player.tsx
+++ components/Player.tsx
@@ -191,73 +191,88 @@ export default function Player() {
       </div>
 
       {/* MOBILE PLAYER */}
-      <div
-        className="block lg:hidden mx-3 rounded-md p-2 bg-gradient-to-b from-blue-600 pb-4"
-        onClick={() => setIsFullScreen(true)}>
-        <ul>
-          <li className="flex items-center gap-2">
-            <img
-              src={currentTrack.imageUrl}
-              alt={currentTrack.name}
-              className="size-12 rounded-md object-cover"
-            />
-            <div className="flex-1">
-              <p className="font-semibold line-clamp-1">{currentTrack.name}</p>
-              <p className="text-sm text-zinc-400">{currentTrack.artist}</p>
+      <DominantColorExtractor imageUrl={currentTrack.imageUrl}>
+        {(dominantColor) => {
+          const gradientBackground = dominantColor
+            ? `linear-gradient(to bottom, ${dominantColor}, #000000)`
+            : "none";
+
+          return (
+            <div
+              className="block lg:hidden mx-3 rounded-md p-2 bg-gradient-to-b from-blue-600 pb-4"
+              style={{ background: gradientBackground }}
+              onClick={() => setIsFullScreen(true)}>
+              <ul>
+                <li className="flex items-center gap-2">
+                  <img
+                    src={currentTrack.imageUrl}
+                    alt={currentTrack.name}
+                    className="size-12 rounded-md object-cover"
+                  />
+                  <div className="flex-1">
+                    <p className="font-semibold line-clamp-1">
+                      {currentTrack.name}
+                    </p>
+                    <p className="text-sm text-zinc-400">
+                      {currentTrack.artist}
+                    </p>
+                  </div>
+                  <div className="flex space-x-3">
+                    <MonitorSpeaker />
+                    <div
+                      onClick={(e) => {
+                        e.stopPropagation(); // Prevent opening fullscreen when liking
+                        toggleLike();
+                      }}
+                      className={`rounded-full size-6 flex items-center justify-center transition-all ${
+                        isLiked ? "bg-blue-500" : "border-white border-2"
+                      }`}>
+                      <Check className="size-4 text-white" />
+                    </div>
+                    <div
+                      onClick={(e) => {
+                        e.stopPropagation();
+                        setIsPlaying(!isPlaying);
+                      }}>
+                      {isPlaying ? (
+                        // pause icon
+                        <svg
+                          xmlns="http://www.w3.org/2000/svg"
+                          viewBox="0 0 24 24"
+                          className="size-6 text-white"
+                          fill="#fff">
+                          <path d="M6 3H8V21H6V3ZM16 3H18V21H16V3Z"></path>
+                        </svg>
+                      ) : (
+                        // play icon
+                        <svg
+                          xmlns="http://www.w3.org/2000/svg"
+                          viewBox="0 0 24 24"
+                          className="size-6"
+                          fill="#fff">
+                          <path d="M6 20.1957V3.80421C6 3.01878 6.86395 2.53993 7.53 2.95621L20.6432 11.152C21.2699 11.5436 21.2699 12.4563 20.6432 12.848L7.53 21.0437C6.86395 21.46 6 20.9812 6 20.1957Z"></path>
+                        </svg>
+                      )}
+                    </div>
+                  </div>
+                </li>
+                <div className="flex flex-col items-center w-full">
+                  <div className="w-full mt-2 flex items-center gap-2">
+                    <input
+                      type="range"
+                      min="0"
+                      max={duration || 0}
+                      value={progress}
+                      onChange={handleProgressChange}
+                      className="w-full h-1 bg-blue-300 rounded-full appearance-none cursor-pointer [&::-webkit-slider-thumb]:appearance-none [&::-webkit-slider-thumb]:w-2 [&::-webkit-slider-thumb]:h-2 [&::-webkit-slider-thumb]:rounded-full [&::-webkit-slider-thumb]:bg-white"
+                    />
+                  </div>
+                </div>
+              </ul>
             </div>
-            <div className="flex space-x-3">
-              <MonitorSpeaker />
-              <div
-                onClick={(e) => {
-                  e.stopPropagation(); // Prevent opening fullscreen when liking
-                  toggleLike();
-                }}
-                className={`rounded-full size-6 flex items-center justify-center transition-all ${
-                  isLiked ? "bg-blue-500" : "border-white border-2"
-                }`}>
-                <Check className="size-4 text-white" />
-              </div>
-              <div
-                onClick={(e) => {
-                  e.stopPropagation();
-                  setIsPlaying(!isPlaying);
-                }}>
-                {isPlaying ? (
-                  // pause icon
-                  <svg
-                    xmlns="http://www.w3.org/2000/svg"
-                    viewBox="0 0 24 24"
-                    className="size-6 text-white"
-                    fill="#fff">
-                    <path d="M6 3H8V21H6V3ZM16 3H18V21H16V3Z"></path>
-                  </svg>
-                ) : (
-                  // play icon
-                  <svg
-                    xmlns="http://www.w3.org/2000/svg"
-                    viewBox="0 0 24 24"
-                    className="size-6"
-                    fill="#fff">
-                    <path d="M6 20.1957V3.80421C6 3.01878 6.86395 2.53993 7.53 2.95621L20.6432 11.152C21.2699 11.5436 21.2699 12.4563 20.6432 12.848L7.53 21.0437C6.86395 21.46 6 20.9812 6 20.1957Z"></path>
-                  </svg>
-                )}
-              </div>
-            </div>
-          </li>
-          <div className="flex flex-col items-center w-full">
-            <div className="w-full mt-2 flex items-center gap-2">
-              <input
-                type="range"
-                min="0"
-                max={duration || 0}
-                value={progress}
-                onChange={handleProgressChange}
-                className="w-full h-1 bg-blue-300 rounded-full appearance-none cursor-pointer [&::-webkit-slider-thumb]:appearance-none [&::-webkit-slider-thumb]:w-2 [&::-webkit-slider-thumb]:h-2 [&::-webkit-slider-thumb]:rounded-full [&::-webkit-slider-thumb]:bg-white"
-              />
-            </div>
-          </div>
-        </ul>
-      </div>
+          );
+        }}
+      </DominantColorExtractor>
 
       {/* MOBILE FULLSCREEN MUSIC PLAYER */}
       {isFullScreen && (
@@ -271,14 +286,15 @@ export default function Player() {
               <div
                 className="h-screen fixed inset-0 flex md:hidden flex-col p-2 z-50"
                 style={{ background: gradientBackground }}>
-                <div className="relative flex items-center justify-center w-full mb-16">
+                {/* Top Section */}
+                <div className="relative flex items-center justify-center w-full mb-8">
                   <button
                     className="absolute left-4 text-white text-2xl"
                     onClick={() => setIsFullScreen(false)}>
                     <ChevronDown className="size-7" />
                   </button>
 
-                  <div className="flex flex-col items-center mt-2">
+                  <div className="flex flex-col items-center">
                     <p className="uppercase text-xs">playing from playlist</p>
                     <span className="font-semibold text-xs">
                       Discover Weekly
@@ -290,8 +306,9 @@ export default function Player() {
                   </button>
                 </div>
 
-                <div className="px-2 ">
-                  <div className="mb-12 flex items-center justify-center">
+                {/* Album Art and Track Info - Centers in remaining space */}
+                <div className="flex-1 flex flex-col justify-center items-center px-2">
+                  <div className="mb-6">
                     <img
                       src={currentTrack.imageUrl}
                       alt={currentTrack.name}
@@ -299,9 +316,8 @@ export default function Player() {
                     />
                   </div>
 
-                  {/* Song Details + Like button */}
-                  <div className="mb-6">
-                    <div className="flex items-center justify-between ">
+                  <div className="w-full mb-4">
+                    <div className="flex items-center justify-between">
                       <div className="flex flex-col">
                         <h2 className="text-white font-bold line-clamp-1">
                           {currentTrack.name}
@@ -319,77 +335,80 @@ export default function Player() {
                       </div>
                     </div>
                   </div>
+                </div>
 
-                  {/* Music Controls */}
-                  <div className="flex flex-col items-center w-full">
-                    <div className="w-full flex flex-col gap-1">
-                      <input
-                        type="range"
-                        min="0"
-                        max={duration || 0}
-                        value={progress}
-                        onChange={handleProgressChange}
-                        className="w-full h-1 bg-zinc-600 rounded-full appearance-none cursor-pointer [&::-webkit-slider-thumb]:appearance-none [&::-webkit-slider-thumb]:w-3 [&::-webkit-slider-thumb]:h-3 [&::-webkit-slider-thumb]:rounded-full [&::-webkit-slider-thumb]:bg-white"
-                      />
-                      <div className="pt-2 w-full flex items-center justify-between text-xs text-gray-400">
-                        <span>{formatTime(progress)}</span>
-                        <span>{formatTime(duration)}</span>
-                      </div>
+                {/* Bottom Controls Section */}
+                <div className="w-full px-2 pb-6">
+                  {/* Progress Bar */}
+                  <div className="w-full flex flex-col gap-1 mb-6">
+                    <input
+                      type="range"
+                      min="0"
+                      max={duration || 0}
+                      value={progress}
+                      onChange={handleProgressChange}
+                      className="w-full h-1 bg-zinc-600 rounded-full appearance-none cursor-pointer [&::-webkit-slider-thumb]:appearance-none [&::-webkit-slider-thumb]:w-3 [&::-webkit-slider-thumb]:h-3 [&::-webkit-slider-thumb]:rounded-full [&::-webkit-slider-thumb]:bg-white"
+                    />
+                    <div className="pt-2 w-full flex items-center justify-between text-xs text-gray-400">
+                      <span>{formatTime(progress)}</span>
+                      <span>{formatTime(duration)}</span>
                     </div>
+                  </div>
 
-                    <div className="flex items-center space-x-6">
-                      <Shuffle className="size-5" />
+                  {/* Playback Controls */}
+                  <div className="flex items-center justify-center space-x-6 mb-4">
+                    <Shuffle className="size-5" />
 
-                      <button className="text-white">
-                        <svg
-                          xmlns="http://www.w3.org/2000/svg"
-                          viewBox="0 0 24 24"
-                          className="size-8 rotate-180"
-                          onClick={skipToPrevious}
-                          fill="#fff">
-                          <path d="M17 6 17 18H15L15 6 17 6ZM13 12 7 18V6L13 12Z"></path>
-                        </svg>
-                      </button>
-                      <button
-                        className="bg-white rounded-full p-2 hover:scale-105 transition"
-                        onClick={() => setIsPlaying(!isPlaying)}>
-                        {isPlaying ? (
-                          // pause icon
-                          <svg
-                            xmlns="http://www.w3.org/2000/svg"
-                            viewBox="0 0 24 24"
-                            className="size-8"
-                            fill="#000">
-                            <path d="M6 3H8V21H6V3ZM16 3H18V21H16V3Z"></path>
-                          </svg>
-                        ) : (
-                          // play icon
-                          <svg
-                            xmlns="http://www.w3.org/2000/svg"
-                            viewBox="0 0 24 24"
-                            className="size-8"
-                            fill="#000">
-                            <path d="M6 20.1957V3.80421C6 3.01878 6.86395 2.53993 7.53 2.95621L20.6432 11.152C21.2699 11.5436 21.2699 12.4563 20.6432 12.848L7.53 21.0437C6.86395 21.46 6 20.9812 6 20.1957Z"></path>
-                          </svg>
-                        )}
-                      </button>
-                      <button className="text-white">
+                    <button className="text-white">
+                      <svg
+                        xmlns="http://www.w3.org/2000/svg"
+                        viewBox="0 0 24 24"
+                        className="size-8 rotate-180"
+                        onClick={skipToPrevious}
+                        fill="#fff">
+                        <path d="M17 6 17 18H15L15 6 17 6ZM13 12 7 18V6L13 12Z" />
+                      </svg>
+                    </button>
+
+                    <button
+                      className="bg-white rounded-full p-2 hover:scale-105 transition"
+                      onClick={() => setIsPlaying(!isPlaying)}>
+                      {isPlaying ? (
                         <svg
                           xmlns="http://www.w3.org/2000/svg"
                           viewBox="0 0 24 24"
                           className="size-8"
-                          onClick={skipToNext}
-                          fill="#fff">
-                          <path d="M17 6 17 18H15L15 6 17 6ZM13 12 7 18V6L13 12Z"></path>
+                          fill="#000">
+                          <path d="M6 3H8V21H6V3ZM16 3H18V21H16V3Z" />
                         </svg>
-                      </button>
+                      ) : (
+                        <svg
+                          xmlns="http://www.w3.org/2000/svg"
+                          viewBox="0 0 24 24"
+                          className="size-8"
+                          fill="#000">
+                          <path d="M6 20.1957V3.80421C6 3.01878 6.86395 2.53993 7.53 2.95621L20.6432 11.152C21.2699 11.5436 21.2699 12.4563 20.6432 12.848L7.53 21.0437C6.86395 21.46 6 20.9812 6 20.1957Z" />
+                        </svg>
+                      )}
+                    </button>
 
-                      <Repeat className="size-5" />
-                    </div>
+                    <button className="text-white">
+                      <svg
+                        xmlns="http://www.w3.org/2000/svg"
+                        viewBox="0 0 24 24"
+                        className="size-8"
+                        onClick={skipToNext}
+                        fill="#fff">
+                        <path d="M17 6 17 18H15L15 6 17 6ZM13 12 7 18V6L13 12Z" />
+                      </svg>
+                    </button>
+
+                    <Repeat className="size-5" />
                   </div>
-                  <div className="mt-2 flex justify-between">
-                    <MonitorSpeaker className="size-5" />
 
+                  {/* Bottom Icons */}
+                  <div className="flex justify-between">
+                    <MonitorSpeaker className="size-5" />
                     <div className="flex gap-6">
                       <Share2 className="size-5" />
                       <Menu className="size-5" />
